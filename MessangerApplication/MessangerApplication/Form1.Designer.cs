1d16c2a65f9b5d7bb0ef4679925a85fb4c29f9ec
윤현준 <kkkths@naver.com>
2020-11-23 00:34:18 +0900

    기본 UI 디자인
    추가

---
+++ MessangerApplication/MessangerApplication/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace MessangerApplication
+{
+    partial class ChattingForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtChatting = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtContents = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtChatting
+            // 
+            this.txtChatting.Location = new System.Drawing.Point(12, 359);
+            this.txtChatting.Name = "txtChatting";
+            this.txtChatting.Size = new System.Drawing.Size(213, 21);
+            this.txtChatting.TabIndex = 0;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(231, 359);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(83, 23);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtContents
+            // 
+            this.txtContents.Location = new System.Drawing.Point(12, 12);
+            this.txtContents.Name = "txtContents";
+            this.txtContents.Size = new System.Drawing.Size(302, 335);
+            this.txtContents.TabIndex = 2;
+            this.txtContents.Text = "";
+            // 
+            // ChattingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 394);
+            this.Controls.Add(this.txtContents);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtChatting);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "ChattingForm";
+            this.Text = "채팅";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtChatting;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.RichTextBox txtContents;
+    }
+}
+
