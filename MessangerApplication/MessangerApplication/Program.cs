7d06bac0a0b0c17c6aa27e6a73e64859e8135b60
윤현준 <kkkths@naver.com>
2020-11-24 07:07:11 +0900

    update MainClass
    update

--- MessangerApplication/MessangerApplication/Program.cs
+++ MessangerApplication/MessangerApplication/Program.cs
@@ -16,7 +16,7 @@ namespace MessangerApplication
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ChattingForm());
+            Application.Run(new LoginForm());
         }
     }
 }
