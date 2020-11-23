10cbdb7215714812ab3af97ee8231c120427bf8f
윤현준 <kkkths@naver.com>
2020-11-24 07:06:56 +0900

    add UserData
    add

---
+++ MessangerApplication/MessangerApplication/UserData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessangerApplication
+{
+    class UserData
+    {
+        private string userId;
+
+        public string getUserId()
+        {
+            return userId;
+        }
+
+        public void setUserId(string userId)
+        {
+            this.userId = userId;
+        }
+    }
+}
