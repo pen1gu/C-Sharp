1fc826f66e37c722e80b9ab1a5ebbb22138bda04
윤현준 <kkkths@naver.com>
2021-02-10 03:12:02 +0900

    create
    program

---
+++ TestStatementSpeed/TestStatementSpeed/Program.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace TestStatementSpeed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (MySqlConnection connection = new MySqlConnection("Server=localhost;Database=Session1;Uid='user';Pwd='1234'"))
+            {
+                string selectQuery = "select * from `member`;";
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(selectQuery, connection);
+                try
+                {
+                    if (command.ExecuteNonQuery() == 1)
+                    {
+                        Console.WriteLine("good");
+                    }
+                    else
+                    {
+                        Console.WriteLine("nope");
+                    }
+                }
+                catch (Exception e) {
+                    Console.WriteLine(e.ToString());       
+                }
+                connection.Close();
+            }
+        }
+    }
+}
