3f482c0435b59d4d37810c96ecc5d0249240fdb8
hyunjun yun <kkkths@naver.com>
2021-02-24 12:16:36 +0900

    create
    random food select form

---
+++ SelectFoodProject/SelectFoodProject/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SelectFoodProject
+{
+    public partial class RandomMenuForm : Form
+    {
+        public RandomMenuForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConfirm_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
