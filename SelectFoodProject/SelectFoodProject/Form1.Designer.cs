c35eb8ce2757af6d73c54e0ca3491d4be6041c0f
hyunjun yun <kkkths@naver.com>
2021-02-24 12:16:42 +0900

    create
    food

---
+++ SelectFoodProject/SelectFoodProject/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace SelectFoodProject
+{
+    partial class RandomMenuForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BtnConfirm = new System.Windows.Forms.Button();
+            this.FoodCheckList = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.BtnCurrentFood = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(202, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(358, 325);
+            this.panel1.TabIndex = 0;
+            // 
+            // BtnConfirm
+            // 
+            this.BtnConfirm.Location = new System.Drawing.Point(468, 354);
+            this.BtnConfirm.Name = "BtnConfirm";
+            this.BtnConfirm.Size = new System.Drawing.Size(92, 25);
+            this.BtnConfirm.TabIndex = 1;
+            this.BtnConfirm.Text = "선택";
+            this.BtnConfirm.UseVisualStyleBackColor = true;
+            this.BtnConfirm.Click += new System.EventHandler(this.BtnConfirm_Click);
+            // 
+            // FoodCheckList
+            // 
+            this.FoodCheckList.FormattingEnabled = true;
+            this.FoodCheckList.Location = new System.Drawing.Point(582, 12);
+            this.FoodCheckList.Name = "FoodCheckList";
+            this.FoodCheckList.Size = new System.Drawing.Size(196, 324);
+            this.FoodCheckList.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "최근 나온 메뉴";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(200, 360);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "메뉴 추가";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(263, 357);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 21);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(63, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // BtnCurrentFood
+            // 
+            this.BtnCurrentFood.Location = new System.Drawing.Point(103, 14);
+            this.BtnCurrentFood.Name = "BtnCurrentFood";
+            this.BtnCurrentFood.Size = new System.Drawing.Size(95, 26);
+            this.BtnCurrentFood.TabIndex = 6;
+            this.BtnCurrentFood.Text = "최근 통계 보기";
+            this.BtnCurrentFood.UseVisualStyleBackColor = true;
+            // 
+            // RandomMenuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 397);
+            this.Controls.Add(this.BtnCurrentFood);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FoodCheckList);
+            this.Controls.Add(this.BtnConfirm);
+            this.Controls.Add(this.panel1);
+            this.Name = "RandomMenuForm";
+            this.Text = "메뉴 랜덤 선택";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button BtnConfirm;
+        private System.Windows.Forms.CheckedListBox FoodCheckList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BtnCurrentFood;
+    }
+}
+
